518adb83ac2c81149d9eff34405f4e6b99fbf0b9
axefx <axellfx@gmail.com>
2020-04-28 17:55:53 -0700

    load model to use in strains_route

--- strainrec_app/routes/strains_routes.py
+++ strainrec_app/routes/strains_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, jsonify
-
-# TODO: import model
+import json
+from strainrec_app.recommender import load_model, data
 
 strains_routes = Blueprint("strains_routes", __name__)
 
@@ -12,9 +12,9 @@ def home():
 
 @strains_routes.route("/strains.json")
 def strains():
-    strains_records = [
-        {'id': 1, 'name': "name1", 'description': "describe name1"}
-    ]
+    print(data)
+    print(dir(data))
+    strains_records = data.to_dict()
     return jsonify(strains_records)
 
 
@@ -22,8 +22,16 @@ def strains():
 def recommend(input_string=None):
     input = {"input": str(input_string)}
     print(input)
-    response = [
-        input,
-        {"recommend": "recommend string"}
-    ]
-    return jsonify(response)
+
+    package = load_model()
+    input_vec = package['tfidf'].transform([str(input_string)])
+    predictions = package['model'].kneighbors(input_vec.todense())
+    recommendations = predictions[1]
+    strains_info = []
+    for i in range(4):
+        info = data.iloc[recommendations[0][i]]
+        strains_info.append(info)
+
+    print("RESULT:", strains_info)
+
+    return jsonify(str(strains_info))
